209649f584a9b50cf5c420a46e801b142cb8e091
iris <iris@serv.pink>
2020-12-01 20:00:27 +0000

    run.sh: rename log to fic_pw_uwsgi.log

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 exec uwsgi --plugin python3 --http-socket 127.0.0.1:9091 --enable-threads \
-	--logto ./uwsgi.log \
+	--logto ./fic_pw_uwsgi.log \
 	--pidfile master.pid --master --processes 2 --threads 2 \
 	--wsgi-file ./main.py --callable app
 

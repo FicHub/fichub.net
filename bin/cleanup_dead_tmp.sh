de6a45bd7d1146bef2464f7752b85bbd291186a9
iris <iris@serv.pink>
2021-06-26 15:41:38 +0200

    bin/cleanup_dead_tmp.sh: remove orphaned /tmp/calibre* files

--- bin/cleanup_dead_tmp.sh
+++ bin/cleanup_dead_tmp.sh
@@ -2,6 +2,14 @@
 # remove tmp/ directories corresponding to dead processes
 
 find tmp/ -mindepth 1 -maxdepth 1 \
-	| grep -vf <(ps -eo pid) \
-	| xargs rm -r
+		| grep -vf <(ps -eo pid) \
+		| while read -r d; do
+	rm -r $d
+done
+
+# if calibre isn't running, clean up any leftover temp dirs older than 5 minutes
+if ! pgrep -f calibre >/dev/null ; then
+	find /tmp -mindepth 1 -maxdepth 1 -type d -name 'calibre*' -cmin +5 -print0 \
+		| xargs -r0 rm -r
+fi
 

8a23a7eca77255b51e159d159d4167860a3a443b
iris <iris@serv.pink>
2021-06-26 15:42:43 +0200

    bin/kill_orphaned_calibre.sh: actually kill

--- bin/kill_orphaned_calibre.sh
+++ bin/kill_orphaned_calibre.sh
@@ -2,5 +2,6 @@
 
 # TODO if everything is an orphan there's probably no space before the initial # 1
 ps xjf -u fichub_net | grep -E '[0-9] /opt/calibre' | tr -s ' ' | grep '^ 1 '
-echo kill -9 $(ps xjf -u fichub_net | grep -E '[0-9] /opt/calibre' | tr -s ' ' | grep '^ 1 ' | cut -d' ' -f3)
+ps xjf -u fichub_net | grep -E '[0-9] /opt/calibre' | tr -s ' ' | grep '^ 1 ' \
+	| cut -d' ' -f3 | xargs -r kill -9
 

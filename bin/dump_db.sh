67e951f827a85a57de8747fcb39ece50440d4c24
iris <iris@serv.pink>
2021-12-30 18:32:09 +0000

    dump_db: compress harder

--- bin/dump_db.sh
+++ bin/dump_db.sh
@@ -5,5 +5,5 @@ dbname="${1-fichub}"
 
 mkdir -p bak/
 
-pg_dump "${dbname}" | gzip > bak/${dbname}_dump_$(date '+%s').sql.gz
+pg_dump "${dbname}" | gzip -9 > bak/${dbname}_dump_$(date '+%s').sql.gz
 
